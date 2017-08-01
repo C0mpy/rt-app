84e49b0f3c0c471255c377a96e709c89fca20f11
C0mpy <n.zunic@yahoo.com>
2017-08-01 10:01:19 +0200

    added test for question service

--- question_server/test/question_server_test.exs
+++ question_server/test/question_server_test.exs
@@ -1,8 +1,14 @@
 defmodule QuestionServerTest do
   use ExUnit.Case
-  doctest QuestionServer
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "service should provide expected values" do
+    response = Question.QuestionService.Server.question_call(Question.QuestionRequest.new(), nil)
+    questions = response.question
+    question1 = Enum.at(questions, 0)
+    question2 = Enum.at(questions, 1)
+    question3 = Enum.at(questions, 2)
+    assert question1 == "what is 5 times 2 ?"
+    assert question2 == "what is 4 minus 1 ?"
+    assert question3 == "what is 7 plus 8 ?"
   end
 end
